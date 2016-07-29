d84857605c4a0796634d4e697370be913afd614a
Vincenzo Acinapura <vin.aci@gmail.com>
2016-07-29 14:11:10 -0700

    Made this thing work, somehow

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -1,5 +1,6 @@
 require 'capybara'
 require 'capybara/rspec'
+require 'capybara-screenshot/rspec'
 require 'capybara/poltergeist'
 require 'selenium-webdriver'
 require 'site_prism'
@@ -8,10 +9,16 @@ class ReactSelectElement < SitePrism::Section
   element :input_element, '.Select-input input'
   elements :options, '.Select-option'
 
-  def select_option(text)
+  def set(text)
     root_element.click
-    input_element.send_keys text
-    options.first.click
+
+    if Capybara.current_driver == :poltergeist
+      root_element.send_keys text
+      root_element.send_keys :enter
+    else
+      input_element.set text
+      input_element.send_keys :enter
+    end
   end
 end
 
@@ -20,20 +27,20 @@ class ReactSelectPage < SitePrism::Page
   section :states_select, ReactSelectElement, '#example > div > div:nth-child(1) > .Select'
 end
 
-describe 'interacting with react-select component' do
+describe 'react-select component' do
   let(:page) { ReactSelectPage.new.tap(&:load) }
 
-  it 'works using Selenium' do
-    Capybara.configure { |c| c.default_driver = :selenium }
+  it 'can be interacted with using Selenium' do
+    Capybara.default_driver = Capybara.javascript_driver = :selenium
 
-    page.states_select.select_option 'Victoria'
+    page.states_select.set 'Victoria'
     expect(page.states_select).to have_text 'Victoria'
   end
 
-  it 'works using Poltergeist' do
-    Capybara.configure { |c| c.default_driver = :poltergeist }
+  it 'can be interacted with using Poltergeist' do
+    Capybara.default_driver = Capybara.javascript_driver = :poltergeist
 
-    page.states_select.select_option 'Victoria'
+    page.states_select.set 'Victoria'
     expect(page.states_select).to have_text 'Victoria'
   end
 end
